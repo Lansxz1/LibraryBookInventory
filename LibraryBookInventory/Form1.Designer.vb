0ad4ffc93416dedb2572b68919ce109175965186
Lansxz1 <lancebulan1928@gmail.com>
2025-12-10 15:30:17 +0800

    Recode: All CRUD buttons are now functional

--- LibraryBookInventory/Form1.Designer.vb
+++ LibraryBookInventory/Form1.Designer.vb
@@ -33,15 +33,12 @@ Partial Class Form1
         Me.Label4 = New System.Windows.Forms.Label()
         Me.btnView = New System.Windows.Forms.Button()
         Me.btnUpdate = New System.Windows.Forms.Button()
-        Me.btnEdit = New System.Windows.Forms.Button()
         Me.btnDelete = New System.Windows.Forms.Button()
         Me.cmbCategory = New System.Windows.Forms.ComboBox()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.cbxAvailable = New System.Windows.Forms.CheckBox()
-        Me.cbxBorrowed = New System.Windows.Forms.CheckBox()
         Me.cbxNA = New System.Windows.Forms.CheckBox()
-        Me.btnConnect = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -128,7 +125,7 @@ Partial Class Form1
         'btnView
         '
         Me.btnView.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.btnView.Location = New System.Drawing.Point(173, 400)
+        Me.btnView.Location = New System.Drawing.Point(226, 400)
         Me.btnView.Name = "btnView"
         Me.btnView.Size = New System.Drawing.Size(101, 38)
         Me.btnView.TabIndex = 9
@@ -138,26 +135,17 @@ Partial Class Form1
         'btnUpdate
         '
         Me.btnUpdate.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.btnUpdate.Location = New System.Drawing.Point(333, 400)
+        Me.btnUpdate.Location = New System.Drawing.Point(436, 400)
         Me.btnUpdate.Name = "btnUpdate"
         Me.btnUpdate.Size = New System.Drawing.Size(101, 38)
         Me.btnUpdate.TabIndex = 10
         Me.btnUpdate.Text = "Update"
         Me.btnUpdate.UseVisualStyleBackColor = False
         '
-        'btnEdit
-        '
-        Me.btnEdit.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.btnEdit.Location = New System.Drawing.Point(502, 400)
-        Me.btnEdit.Name = "btnEdit"
-        Me.btnEdit.Size = New System.Drawing.Size(101, 38)
-        Me.btnEdit.TabIndex = 11
-        Me.btnEdit.Text = "Edit"
-        Me.btnEdit.UseVisualStyleBackColor = False
-        '
         'btnDelete
         '
-        Me.btnDelete.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnDelete.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnDelete.ForeColor = System.Drawing.Color.White
         Me.btnDelete.Location = New System.Drawing.Point(666, 400)
         Me.btnDelete.Name = "btnDelete"
         Me.btnDelete.Size = New System.Drawing.Size(101, 38)
@@ -204,34 +192,15 @@ Partial Class Form1
         Me.cbxAvailable.Text = "Available"
         Me.cbxAvailable.UseVisualStyleBackColor = True
         '
-        'cbxBorrowed
-        '
-        Me.cbxBorrowed.AutoSize = True
-        Me.cbxBorrowed.Location = New System.Drawing.Point(644, 104)
-        Me.cbxBorrowed.Name = "cbxBorrowed"
-        Me.cbxBorrowed.Size = New System.Drawing.Size(71, 17)
-        Me.cbxBorrowed.TabIndex = 17
-        Me.cbxBorrowed.Text = "Borrowed"
-        Me.cbxBorrowed.UseVisualStyleBackColor = True
-        '
         'cbxNA
         '
         Me.cbxNA.AutoSize = True
-        Me.cbxNA.Location = New System.Drawing.Point(721, 104)
+        Me.cbxNA.Location = New System.Drawing.Point(652, 104)
         Me.cbxNA.Name = "cbxNA"
         Me.cbxNA.Size = New System.Drawing.Size(46, 17)
         Me.cbxNA.TabIndex = 18
         Me.cbxNA.Text = "N/A"
         Me.cbxNA.UseVisualStyleBackColor = True
-        '
-        'btnConnect
-        '
-        Me.btnConnect.Location = New System.Drawing.Point(12, 9)
-        Me.btnConnect.Name = "btnConnect"
-        Me.btnConnect.Size = New System.Drawing.Size(83, 27)
-        Me.btnConnect.TabIndex = 19
-        Me.btnConnect.Text = "Connect"
-        Me.btnConnect.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -239,15 +208,12 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.Menu
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.btnConnect)
         Me.Controls.Add(Me.cbxNA)
-        Me.Controls.Add(Me.cbxBorrowed)
         Me.Controls.Add(Me.cbxAvailable)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.cmbCategory)
         Me.Controls.Add(Me.btnDelete)
-        Me.Controls.Add(Me.btnEdit)
         Me.Controls.Add(Me.btnUpdate)
         Me.Controls.Add(Me.btnView)
         Me.Controls.Add(Me.Label4)
@@ -278,13 +244,10 @@ Partial Class Form1
     Friend WithEvents Label4 As Label
     Friend WithEvents btnView As Button
     Friend WithEvents btnUpdate As Button
-    Friend WithEvents btnEdit As Button
     Friend WithEvents btnDelete As Button
     Friend WithEvents cmbCategory As ComboBox
     Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
     Friend WithEvents cbxAvailable As CheckBox
-    Friend WithEvents cbxBorrowed As CheckBox
     Friend WithEvents cbxNA As CheckBox
-    Friend WithEvents btnConnect As Button
 End Class
--- LibraryBookInventory/Form1.vb
+++ LibraryBookInventory/Form1.vb
@@ -1,56 +1,205 @@
-﻿Imports System.Data.SqlClient
-Imports MySql.Data.MySqlClient
+﻿Imports MySql.Data.MySqlClient
+
 Public Class Form1
+
     Dim conn As MySqlConnection
     Dim COMMAND As MySqlCommand
-    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
-        conn = New MySqlConnection
-        conn.ConnectionString = "server=localhost; userid=root; password=root; database= library_inventory"
 
-        Try
-            conn.Open()
-            MessageBox.Show("Connected")
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
-            conn.Close()
-        End Try
-    End Sub
+
     Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
-        Dim query As String = "INSERT INTO library_tbl (id, title, author) VALUES (@id, @title, @author)"
-
         Try
-            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database= library_inventory")
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=library_inventory")
                 conn.Open()
-            End Using
-            Using cmd As New MySqlCommand(query, conn)
-                cmd.Parameters.AddWithValue("@id", tbID.Text)
-                cmd.Parameters.AddWithValue("@title", tbTitle.Text)
-                cmd.Parameters.AddWithValue("@author", tbAuthor.Text)
-                cmd.ExecuteNonQuery()
-                MessageBox.Show("Record insert successful", "Record Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
 
+                If tbID.Text <> String.Empty And
+                   tbTitle.Text <> String.Empty And
+                   tbAuthor.Text <> String.Empty And
+                   cmbCategory.Text <> String.Empty Then
+
+                    Dim checkQuery As String = "SELECT COUNT(*) FROM library_tbl WHERE id = @id"
+                    Using checkCmd As New MySqlCommand(checkQuery, conn)
+                        checkCmd.Parameters.AddWithValue("@id", tbID.Text)
+                        Dim count As Integer = Convert.ToInt32(checkCmd.ExecuteScalar())
+
+                        If count > 0 Then
+                            MessageBox.Show("ID already exists. Please enter a unique ID.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                            tbID.Focus()
+                            Return
+                        End If
+                    End Using
+
+                    Dim availability As String = ""
+                    If cbxAvailable.Checked Then
+                        availability = "Available"
+                    ElseIf cbxNA.Checked Then
+                        availability = "Not Available"
+                    Else
+                        MessageBox.Show("Please select if Available/Not Available.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                        Exit Sub
+                    End If
+
+                    Dim query As String = "INSERT INTO library_tbl (id, title, author, category, availability, is_deleted) VALUES (@id, @title, @author, @category, @availability, 0)"
+                    Using cmd As New MySqlCommand(query, conn)
+                        cmd.Parameters.AddWithValue("@id", tbID.Text)
+                        cmd.Parameters.AddWithValue("@title", tbTitle.Text)
+                        cmd.Parameters.AddWithValue("@author", tbAuthor.Text)
+                        cmd.Parameters.AddWithValue("@category", cmbCategory.Text)
+                        cmd.Parameters.AddWithValue("@availability", availability)
+
+                        cmd.ExecuteNonQuery()
+                        MessageBox.Show("Book added to your inventory.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+                        tbID.Clear()
+                        tbTitle.Clear()
+                        tbAuthor.Clear()
+                        cmbCategory.SelectedIndex = -1
+                        cbxAvailable.Checked = False
+                        cbxNA.Checked = False
+                        RefreshGrid()
+                    End Using
+
+                Else
+                    MessageBox.Show("Please fill out all the needed information", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
             End Using
         Catch ex As Exception
-            MessageBox.Show(ex.Message)
+            MsgBox(ex.Message)
         End Try
     End Sub
-
 
     Private Sub btnView_Click(sender As Object, e As EventArgs) Handles btnView.Click
+        Dim query As String = "SELECT id, title, author, category, availability FROM library_tbl WHERE is_deleted = 0"
 
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=library_inventory")
+                Dim adapter As New MySqlDataAdapter(query, conn)
+                Dim table As New DataTable()
+
+                adapter.Fill(table)
+                DataGridView1.DataSource = table
+
+                tbID.Clear()
+                tbTitle.Clear()
+                tbAuthor.Clear()
+                cmbCategory.SelectedIndex = -1
+                cbxAvailable.Checked = False
+                cbxNA.Checked = False
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
     End Sub
 
     Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        If tbID.Text = "" Then
+            MessageBox.Show("Double click a book info to update", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
 
-    End Sub
+        Dim query As String = "UPDATE library_tbl SET title = @title, author = @author, category = @category, availability = @availability WHERE id = @id"
 
-    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=library_inventory")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
 
+                    Dim availability As String = ""
+                    If cbxAvailable.Checked Then
+                        availability = "Available"
+                    Else
+                        availability = "Not Available"
+                    End If
+
+                    cmd.Parameters.AddWithValue("@id", CInt(tbID.Text))
+                    cmd.Parameters.AddWithValue("@title", tbTitle.Text)
+                    cmd.Parameters.AddWithValue("@author", tbAuthor.Text)
+                    cmd.Parameters.AddWithValue("@category", cmbCategory.Text)
+                    cmd.Parameters.AddWithValue("@availability", availability)
+
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Book info updated successfully.", "Updated", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+                    RefreshGrid()
+                    tbID.Clear()
+                    tbTitle.Clear()
+                    tbAuthor.Clear()
+                    cmbCategory.SelectedIndex = -1
+                    cbxAvailable.Checked = False
+                    cbxNA.Checked = False
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
     End Sub
 
     Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If DataGridView1.CurrentRow Is Nothing Then
+            MessageBox.Show("Select a row to delete", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
 
+        Dim selectedID As Integer = CInt(DataGridView1.CurrentRow.Cells("id").Value)
+
+        Dim confirm As DialogResult = MessageBox.Show("Are you sure you want to delete this book?", "Delete?", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If confirm <> DialogResult.Yes Then
+            Return
+        End If
+
+        Dim query As String = "UPDATE library_tbl SET is_deleted = 1 WHERE id = @id"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=library_inventory")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@id", selectedID)
+                    cmd.ExecuteNonQuery()
+                End Using
+            End Using
+
+            MessageBox.Show("Book deleted successfully", "Deleted", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            RefreshGrid()
+            tbID.Clear()
+            tbTitle.Clear()
+            tbAuthor.Clear()
+            cmbCategory.SelectedIndex = -1
+            cbxAvailable.Checked = False
+            cbxNA.Checked = False
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
     End Sub
 
+    Private Sub RefreshGrid()
+        Dim query As String = "SELECT id, title, author, category, availability FROM library_tbl WHERE is_deleted = 0"
+
+        Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=library_inventory")
+            Dim adapter As New MySqlDataAdapter(query, conn)
+            Dim table As New DataTable()
+            adapter.Fill(table)
+            DataGridView1.DataSource = table
+        End Using
+    End Sub
+
+    Private Sub DataGridView1_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellDoubleClick
+        If e.RowIndex >= 0 Then
+            Dim selectedRow As DataGridViewRow = DataGridView1.Rows(e.RowIndex)
+
+            tbID.Text = selectedRow.Cells("id").Value.ToString()
+            tbTitle.Text = selectedRow.Cells("title").Value.ToString()
+            tbAuthor.Text = selectedRow.Cells("author").Value.ToString()
+            cmbCategory.Text = selectedRow.Cells("category").Value.ToString()
+
+            Dim availability As String = selectedRow.Cells("availability").Value.ToString()
+            If availability = "Available" Then
+                cbxAvailable.Checked = True
+                cbxNA.Checked = False
+            Else
+                cbxAvailable.Checked = False
+                cbxNA.Checked = True
+            End If
+        End If
+    End Sub
 
 End Class
